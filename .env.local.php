c0d6ce9d496dc8a0d499d1dcca9dc3310aab4084
Hung Nguyen <hung.nguyen@codebox.vn>
2015-10-04 13:11:22 +0700

    no message

---
+++ .env.local.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'Domain_Name' => 'http://giaymaster.dev:81/',
+    'News' => 1,
+    'Sale' => 2,
+    'Product_Number_Home' => 35,
+    'Product_Number_Product_List' => 24,
+    'Add_Cart_Quantity' => 5,
+    'Login_Fail_Message' => 'Tài khoản đăng nhập không đúng.',
+    'Cart_Empty_Message' => 'Hiện tại chưa có sản phẩm nào trong giỏ hàng.'
+);
